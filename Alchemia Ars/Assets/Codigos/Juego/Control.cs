63600b42c35956b168e200fec95876c6d1c2fef0
Aenisma12 <ferbergona@gmail.com>
2023-10-17 19:09:00 +0200

    Sprite interfaz

--- Alchemia Ars/Assets/Codigos/Juego/Control.cs
+++ Alchemia Ars/Assets/Codigos/Juego/Control.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static Ingrediente;
 using static Pocion;
 
@@ -149,6 +150,11 @@ public class Control : MonoBehaviour
     public void CambiarPuntaje(int puntaje)
     {
         puntajeFinal += puntaje;
+        if (puntajeFinal <= 0)
+        {
+            DontDestroyOnLoad(this);
+            SceneManager.LoadScene("PantallaFinal");
+        }
     }
     /// <summary>
     /// Clase de los ingredeintes incompatibles
