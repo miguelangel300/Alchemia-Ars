f25b1e3a25909557cfbc4ca9a00b5125e4a13a5d
Aenisma12 <ferbergona@gmail.com>
2023-10-20 19:40:25 +0200

    animaciones

--- Alchemia Ars/Assets/Codigos/Seleccionar.cs
+++ Alchemia Ars/Assets/Codigos/Seleccionar.cs
@@ -8,6 +8,33 @@ public class Seleccionar : MonoBehaviour
     //Ingrediente del plato
     public GameObject imagen;
     public static bool start;
+    public Sprite[] imagenes;
+    private SpriteRenderer ingrediente;
+    private int indice;
+    [Range(0f, 1f)]
+    public float velocidad = 0.1f;
+    private void Start()
+    {
+        ingrediente = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
+        indice = 0;
+    }
+    private void OnMouseOver()
+    {
+        InvokeRepeating("CambiarImagen", 0, velocidad);
+    }
+    private void OnMouseExit()
+    {
+        CancelInvoke("CambiarImagen");
+    }
+    private void CambiarImagen()
+    {
+        indice++;
+        if (indice >= imagenes.Length)
+        {
+            indice = 0;
+        }
+        ingrediente.sprite = imagenes[indice];
+    }
     private void OnMouseDown()
     {
         if (start)
